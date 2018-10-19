d823647dcc5936f6d7ba3c476806ba7702b2d36d
doobin <doobin42@gmail.com>
2018-10-18 23:28:41 -0400

    Created curl-script file -Sign up, sign in, change password, sign out, index, show, create, destroy, update

---
+++ curl-scripts/create.sh
@@ -0,0 +1,14 @@
+curl "http://localhost:4741/logs" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "log": {
+      "date": "'"${DATE}"'",
+      "feet": "'"${FEET}"'",
+      "activity": "'"${ACTIVITY}"'"
+    }
+  }'
+
+echo
---
+++ curl-scripts/update.sh
@@ -0,0 +1,14 @@
+curl "http://localhost:4741/logs/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "log": {
+      "date": "'"${DATE}"'",
+      "feet": "'"${FEET}"'",
+      "activity": "'"${ACTIVITY}"'"
+    }
+  }'
+
+echo
