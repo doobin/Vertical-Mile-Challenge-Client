4d1dc5e4ec71d795b00b031f80fdface775379ae
doobin <doobin42@gmail.com>
2018-10-23 09:50:21 -0400

    Changed production URL on config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://verticle-mile-challenge.herokuapp.com',
+  production: 'https://vertical-mile-challenge.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
